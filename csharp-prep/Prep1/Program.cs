86a5802be57af28bfef45b4214e10fab69433470
Hayden Harris <haydenh@Haydens-MacBook-Pro-3.local>
2024-04-25 14:49:22 -0600

    Submitted assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,11 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
-}
+Console.Write("What is your first name?" );
+string first_name = Console.ReadLine();
+
+Console.WriteLine("What is your last name? ");
+string last_name = Console.ReadLine();
+
+Console.WriteLine($"My name is {last_name}, {first_name} {last_name}");
+
+
