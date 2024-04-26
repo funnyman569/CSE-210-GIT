d1ce79628c253d22ca6a691c5f971e5cbb5b3bba
Hayden Harris <haydenh@Haydens-MacBook-Pro-3.local>
2024-04-26 17:06:01 -0600

    Submit assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,11 +1,13 @@
 using System;
 
-Console.Write("What is your first name?" );
+Console.Write("What is your first name? ");
 string first_name = Console.ReadLine();
 
-Console.WriteLine("What is your last name? ");
+Console.Write("What is your last name? ");
 string last_name = Console.ReadLine();
 
 Console.WriteLine($"My name is {last_name}, {first_name} {last_name}");
+
+
 
 
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Runtime.ConstrainedExecution;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+Console.WriteLine("What is your grade percentage? ");
+int grade_percentage = int.Parse(Console.ReadLine());
+string grade = "No answer yet";
+if(grade_percentage >= 90)
+{  
+    grade = "A";
 }
+else if (grade_percentage >= 80 && grade_percentage < 90)
+{
+    grade = "B";
+}  
+else if (grade_percentage >= 70 && grade_percentage < 80)
+{
+    grade = "C";
+}
+else if (grade_percentage >= 60 && grade_percentage < 70)
+{
+    grade = "D";
+}
+else
+{
+    grade = "F";
+}
+
+if (grade_percentage >= 70)
+{
+    Console.WriteLine("Congrats you passed the class");
+}
+else
+{
+    Console.WriteLine("You did not pass. Go find a smart person and hang out with them.");
+}
+
+string plus_or_minus = "";
+double remainder = grade_percentage % 10;
+if (remainder >= 7)
+{
+    plus_or_minus = "+";
+}
+else if (remainder < 3)
+{
+    plus_or_minus = "-";
+}
+
+Console.WriteLine($"Your final grade is {grade}{plus_or_minus}.");
