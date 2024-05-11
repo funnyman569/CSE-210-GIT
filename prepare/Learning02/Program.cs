9e713139ded57b80446e69617a816b12fb601101
Hayden Harris <haydenandamyharris@gmail.com>
2024-05-10 23:04:18 -0600

    Assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Data;
+using System.Runtime.InteropServices;
 
-class Program
+
+
+class Program()
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
+    { 
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Software Engineer";
+        job1._endYear = "2024";
+        job1._startYear = "2000";
+
+        Job job2 = new();
+        job2._company = "Bing";
+        job2._jobTitle = "Software Engineer";
+        job2._endYear = "2000";
+        job2._startYear = "1980";
+
+        Resume myResuem = new();
+        myResuem._name = "Alex";
+        myResuem._jobs.Add(job1);
+        myResuem._jobs.Add(job2);
+
+        myResuem.Display();
+            
+        
     }
 }
+
+
+
+
+
